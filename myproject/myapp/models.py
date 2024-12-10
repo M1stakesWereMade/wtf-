c7999ed5108f7e0880915929be2c72204fe3dde8
Not_Enough <superlexlive@gmail.com>
2024-12-10 16:37:04 +0300

    Add files via upload

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
-class Article(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    author = models.CharField(max_length=100)
-    published_date = models.DateField()
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.PositiveIntegerField()
 
     def __str__(self):
         return self.title
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
